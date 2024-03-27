43d7c2768af8d545dcd84752fc018a048f196081
PR5GRAMM2R <pon07151@gmail.com>
2024-03-27 14:00:02 +0900

    --all

---
+++ 12. Making Notepad/12. Making Memo/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace _12.Making_Memo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mns = new System.Windows.Forms.MenuStrip();
+            this.cld = new System.Windows.Forms.ColorDialog();
+            this.fnd = new System.Windows.Forms.FontDialog();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.sfd = new System.Windows.Forms.SaveFileDialog();
+            this.파일FToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.편집EToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.서식OToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtNotepad = new System.Windows.Forms.TextBox();
+            this.tsmiNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmiExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiUndo = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmiCut = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiDelete = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmiSelectAll = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiWordWrap = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiFont = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmiFontColor = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiBackColor = new System.Windows.Forms.ToolStripMenuItem();
+            this.mns.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mns
+            // 
+            this.mns.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.mns.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일FToolStripMenuItem,
+            this.편집EToolStripMenuItem,
+            this.서식OToolStripMenuItem});
+            this.mns.Location = new System.Drawing.Point(0, 0);
+            this.mns.Name = "mns";
+            this.mns.Size = new System.Drawing.Size(1112, 38);
+            this.mns.TabIndex = 0;
+            this.mns.Text = "menuStrip1";
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "openFileDialog1";
+            // 
+            // 파일FToolStripMenuItem
+            // 
+            this.파일FToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiNew,
+            this.tsmiOpen,
+            this.tsmiSave,
+            this.tsmiSaveAs,
+            this.toolStripMenuItem1,
+            this.tsmiExit});
+            this.파일FToolStripMenuItem.Name = "파일FToolStripMenuItem";
+            this.파일FToolStripMenuItem.Size = new System.Drawing.Size(102, 34);
+            this.파일FToolStripMenuItem.Text = "파일 (&F)";
+            // 
+            // 편집EToolStripMenuItem
+            // 
+            this.편집EToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiUndo,
+            this.toolStripMenuItem2,
+            this.tsmiCut,
+            this.tsmiCopy,
+            this.tsmiPaste,
+            this.tsmiDelete,
+            this.toolStripMenuItem3,
+            this.tsmiSelectAll});
+            this.편집EToolStripMenuItem.Name = "편집EToolStripMenuItem";
+            this.편집EToolStripMenuItem.Size = new System.Drawing.Size(103, 34);
+            this.편집EToolStripMenuItem.Text = "편집 (&E)";
+            // 
+            // 서식OToolStripMenuItem
+            // 
+            this.서식OToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiWordWrap,
+            this.tsmiFont,
+            this.toolStripMenuItem4,
+            this.tsmiFontColor,
+            this.tsmiBackColor});
+            this.서식OToolStripMenuItem.Name = "서식OToolStripMenuItem";
+            this.서식OToolStripMenuItem.Size = new System.Drawing.Size(108, 34);
+            this.서식OToolStripMenuItem.Text = "서식 (&O)";
+            // 
+            // txtNotepad
+            // 
+            this.txtNotepad.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtNotepad.Location = new System.Drawing.Point(0, 38);
+            this.txtNotepad.Multiline = true;
+            this.txtNotepad.Name = "txtNotepad";
+            this.txtNotepad.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtNotepad.Size = new System.Drawing.Size(1112, 626);
+            this.txtNotepad.TabIndex = 1;
+            this.txtNotepad.WordWrap = false;
+            // 
+            // tsmiNew
+            // 
+            this.tsmiNew.Name = "tsmiNew";
+            this.tsmiNew.Size = new System.Drawing.Size(346, 40);
+            this.tsmiNew.Text = "새로 만들기 (&N)";
+            this.tsmiNew.Click += new System.EventHandler(this.tsmiNew_Click);
+            // 
+            // tsmiOpen
+            // 
+            this.tsmiOpen.Name = "tsmiOpen";
+            this.tsmiOpen.Size = new System.Drawing.Size(346, 40);
+            this.tsmiOpen.Text = "열기 (&O) . . .";
+            this.tsmiOpen.Click += new System.EventHandler(this.tsmiOpen_Click);
+            // 
+            // tsmiSave
+            // 
+            this.tsmiSave.Name = "tsmiSave";
+            this.tsmiSave.Size = new System.Drawing.Size(346, 40);
+            this.tsmiSave.Text = "저장 (&S)";
+            this.tsmiSave.Click += new System.EventHandler(this.tsmiSave_Click);
+            // 
+            // tsmiSaveAs
+            // 
+            this.tsmiSaveAs.Name = "tsmiSaveAs";
+            this.tsmiSaveAs.Size = new System.Drawing.Size(346, 40);
+            this.tsmiSaveAs.Text = "다른 이름으로 저장 (&A)";
+            this.tsmiSaveAs.Click += new System.EventHandler(this.tsmiSaveAs_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(343, 6);
+            // 
+            // tsmiExit
+            // 
+            this.tsmiExit.Name = "tsmiExit";
+            this.tsmiExit.Size = new System.Drawing.Size(346, 40);
+            this.tsmiExit.Text = "끝내기 (&X)";
+            this.tsmiExit.Click += new System.EventHandler(this.tsmiExit_Click);
+            // 
+            // tsmiUndo
+            // 
+            this.tsmiUndo.Name = "tsmiUndo";
+            this.tsmiUndo.Size = new System.Drawing.Size(315, 40);
+            this.tsmiUndo.Text = "실행 취소 (&U)";
+            this.tsmiUndo.Click += new System.EventHandler(this.tsmiUndo_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(312, 6);
+            // 
+            // tsmiCut
+            // 
+            this.tsmiCut.Name = "tsmiCut";
+            this.tsmiCut.Size = new System.Drawing.Size(315, 40);
+            this.tsmiCut.Text = "잘라내기 (&T)";
+            this.tsmiCut.Click += new System.EventHandler(this.tsmiCut_Click);
+            // 
+            // tsmiCopy
+            // 
+            this.tsmiCopy.Name = "tsmiCopy";
+            this.tsmiCopy.Size = new System.Drawing.Size(315, 40);
+            this.tsmiCopy.Text = "복사 (&C)";
+            this.tsmiCopy.Click += new System.EventHandler(this.tsmiCopy_Click);
+            // 
+            // tsmiPaste
+            // 
+            this.tsmiPaste.Name = "tsmiPaste";
+            this.tsmiPaste.Size = new System.Drawing.Size(315, 40);
+            this.tsmiPaste.Text = "불여넣기 (&P)";
+            this.tsmiPaste.Click += new System.EventHandler(this.tsmiPaste_Click);
+            // 
+            // tsmiDelete
+            // 
+            this.tsmiDelete.Name = "tsmiDelete";
+            this.tsmiDelete.Size = new System.Drawing.Size(315, 40);
+            this.tsmiDelete.Text = "삭제 (&D)";
+            this.tsmiDelete.Click += new System.EventHandler(this.tsmiDelete_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(312, 6);
+            // 
+            // tsmiSelectAll
+            // 
+            this.tsmiSelectAll.Name = "tsmiSelectAll";
+            this.tsmiSelectAll.Size = new System.Drawing.Size(315, 40);
+            this.tsmiSelectAll.Text = "모두 선택 (&A)";
+            this.tsmiSelectAll.Click += new System.EventHandler(this.tsmiSelectAll_Click);
+            // 
+            // tsmiWordWrap
+            // 
+            this.tsmiWordWrap.Name = "tsmiWordWrap";
+            this.tsmiWordWrap.Size = new System.Drawing.Size(315, 40);
+            this.tsmiWordWrap.Text = "자동 줄 바꿈 (&W)";
+            this.tsmiWordWrap.Click += new System.EventHandler(this.tsmiWordWrap_Click);
+            // 
+            // tsmiFont
+            // 
+            this.tsmiFont.Name = "tsmiFont";
+            this.tsmiFont.Size = new System.Drawing.Size(315, 40);
+            this.tsmiFont.Text = "글꼴 (&F)";
+            this.tsmiFont.Click += new System.EventHandler(this.tsmiFont_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(312, 6);
+            // 
+            // tsmiFontColor
+            // 
+            this.tsmiFontColor.Name = "tsmiFontColor";
+            this.tsmiFontColor.Size = new System.Drawing.Size(315, 40);
+            this.tsmiFontColor.Text = "글자색 바꾸기 (&C)";
+            this.tsmiFontColor.Click += new System.EventHandler(this.tsmiFontColor_Click);
+            // 
+            // tsmiBackColor
+            // 
+            this.tsmiBackColor.Name = "tsmiBackColor";
+            this.tsmiBackColor.Size = new System.Drawing.Size(315, 40);
+            this.tsmiBackColor.Text = "바탕색 바꾸기 (&B)";
+            this.tsmiBackColor.Click += new System.EventHandler(this.tsmiBackColor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1112, 664);
+            this.Controls.Add(this.txtNotepad);
+            this.Controls.Add(this.mns);
+            this.MainMenuStrip = this.mns;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mns.ResumeLayout(false);
+            this.mns.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mns;
+        private System.Windows.Forms.ToolStripMenuItem 파일FToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 편집EToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 서식OToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog cld;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.SaveFileDialog sfd;
+        private System.Windows.Forms.TextBox txtNotepad;
+        private System.Windows.Forms.ToolStripMenuItem tsmiNew;
+        private System.Windows.Forms.ToolStripMenuItem tsmiOpen;
+        private System.Windows.Forms.ToolStripMenuItem tsmiSave;
+        private System.Windows.Forms.ToolStripMenuItem tsmiSaveAs;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiExit;
+        private System.Windows.Forms.ToolStripMenuItem tsmiUndo;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem tsmiCut;
+        private System.Windows.Forms.ToolStripMenuItem tsmiCopy;
+        private System.Windows.Forms.ToolStripMenuItem tsmiPaste;
+        private System.Windows.Forms.ToolStripMenuItem tsmiDelete;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem tsmiSelectAll;
+        private System.Windows.Forms.ToolStripMenuItem tsmiWordWrap;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFont;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFontColor;
+        private System.Windows.Forms.ToolStripMenuItem tsmiBackColor;
+        private System.Windows.Forms.FontDialog fnd;
+    }
+}
+
