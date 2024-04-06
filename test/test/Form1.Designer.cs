9ba345dfc0d9559465f6c6b44525e28847895641
Juhwan Bae <bjhstar0201@gmail.com>
2024-04-06 14:21:49 +0900

    -al

--- test/test/Form1.Designer.cs
+++ test/test/Form1.Designer.cs
@@ -28,20 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rectangleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.circleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileFToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.toolToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1221, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileFToolStripMenuItem
+            // 
+            this.fileFToolStripMenuItem.Name = "fileFToolStripMenuItem";
+            this.fileFToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.fileFToolStripMenuItem.Text = "File (&F)";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.undoToolStripMenuItem,
+            this.redoToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.editToolStripMenuItem.Text = "Edit (&E)";
+            // 
+            // toolToolStripMenuItem
+            // 
+            this.toolToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rectangleToolStripMenuItem,
+            this.circleToolStripMenuItem,
+            this.lineToolStripMenuItem});
+            this.toolToolStripMenuItem.Name = "toolToolStripMenuItem";
+            this.toolToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.toolToolStripMenuItem.Text = "Tool (&T)";
+            // 
+            // undoToolStripMenuItem
+            // 
+            this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.undoToolStripMenuItem.Text = "Undo (&U)";
+            this.undoToolStripMenuItem.Click += new System.EventHandler(this.undoToolStripMenuItem_Click);
+            // 
+            // redoToolStripMenuItem
+            // 
+            this.redoToolStripMenuItem.Name = "redoToolStripMenuItem";
+            this.redoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.redoToolStripMenuItem.Text = "Redo (&R)";
+            this.redoToolStripMenuItem.Click += new System.EventHandler(this.redoToolStripMenuItem_Click);
+            // 
+            // rectangleToolStripMenuItem
+            // 
+            this.rectangleToolStripMenuItem.Name = "rectangleToolStripMenuItem";
+            this.rectangleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.rectangleToolStripMenuItem.Text = "Rectangle (&R)";
+            this.rectangleToolStripMenuItem.Click += new System.EventHandler(this.rectangleToolStripMenuItem_Click);
+            // 
+            // circleToolStripMenuItem
+            // 
+            this.circleToolStripMenuItem.Name = "circleToolStripMenuItem";
+            this.circleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.circleToolStripMenuItem.Text = "Circle (&C)";
+            this.circleToolStripMenuItem.Click += new System.EventHandler(this.circleToolStripMenuItem_Click);
+            // 
+            // lineToolStripMenuItem
+            // 
+            this.lineToolStripMenuItem.Name = "lineToolStripMenuItem";
+            this.lineToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.lineToolStripMenuItem.Text = "Line (&L)";
+            this.lineToolStripMenuItem.Click += new System.EventHandler(this.lineToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1197, 696);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1221, 735);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem undoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rectangleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem circleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lineToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- test/test/Form1.cs
+++ test/test/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,299 @@ namespace test
 {
     public partial class Form1 : Form
     {
+        private static Point clickPoint;
+        private static Point upPoint;
+        private static Bitmap originalBmp;
+        private static Bitmap drawBmp;
+        private static Rectangle imgRect;
+
+        public static PaintTools tooltype {  get; set; }
+        public enum PaintTools
+        {
+            IDLE = default,
+            Drawline,
+            DrawRectangle,
+            DrawCircle
+        }
+
+        public List<Rectangle> listRect = new List<Rectangle>();
+        public List<Rectangle> tempRect = new List<Rectangle>();
+        public List<PaintTools> listTool = new List<PaintTools>();
+        public List<PaintTools> tempTool = new List<PaintTools>();
+
         public Form1()
         {
             InitializeComponent();
+
+            pictureBox1.Image = new Bitmap(Application.StartupPath + @"\DefaultBackGround.png");
+            originalBmp = (Bitmap)pictureBox1.Image;
+            imgRect = new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void rectangleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tooltype = PaintTools.DrawRectangle;
+        }
+
+        private void circleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tooltype = PaintTools.DrawCircle;
+        }
+
+        private void lineToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tooltype = PaintTools.Drawline;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                clickPoint = new Point(e.X, e.Y);
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                float w = Math.Abs(clickPoint.X - e.X);
+                float h = Math.Abs(clickPoint.Y - e.Y);
+
+                Pen pn = new Pen(Color.Black);
+                pn.Width = 3;
+                Graphics g = pictureBox1.CreateGraphics();
+                pictureBox1.Refresh();
+
+                if(tooltype == PaintTools.DrawRectangle)
+                {
+                    if(e.X > clickPoint.X)
+                    {
+                        if(e.Y > clickPoint.Y)
+                        {
+                            g.DrawRectangle(pn, clickPoint.X, clickPoint.Y, w, h);
+                        }
+                        else
+                        {
+                            g.DrawRectangle(pn, clickPoint.X, e.Y, w, h);
+                        }
+                    }
+                    else
+                    {
+                        if (e.Y > clickPoint.Y)
+                        {
+                            g.DrawRectangle(pn, e.X, clickPoint.Y, w, h);
+                        }
+                        else
+                        {
+                            g.DrawRectangle(pn, e.X, e.Y, w, h);
+                        }
+                    }
+                }
+                else if (tooltype == PaintTools.DrawCircle)
+                {
+                    if (e.X > clickPoint.X)
+                    {
+                        if (e.Y > clickPoint.Y)
+                        {
+                            g.DrawEllipse(pn, clickPoint.X, clickPoint.Y, w, h);
+                        }
+                        else
+                        {
+                            g.DrawEllipse(pn, clickPoint.X, e.Y, w, h);
+                        }
+                    }
+                    else
+                    {
+                        if (e.Y > clickPoint.Y)
+                        {
+                            g.DrawEllipse(pn, e.X, clickPoint.Y, w, h);
+                        }
+                        else
+                        {
+                            g.DrawEllipse(pn, e.X, e.Y, w, h);
+                        }
+                    }
+                }
+                else if(tooltype == PaintTools.Drawline)
+                {
+                    g.DrawLine(pn, clickPoint.X, clickPoint.Y, e.X, e.Y);
+                }
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                upPoint.X = e.X;
+                upPoint.Y = e.Y;
+
+                float w = Math.Abs(clickPoint.X - e.X);
+                float h = Math.Abs(clickPoint.Y - e.Y);
+
+                Pen pn = new Pen(Color.Black);
+                pn.Width = 3;
+                Rectangle rect = new Rectangle();
+                Graphics g = pictureBox1.CreateGraphics();
+
+                if(tooltype == PaintTools.DrawRectangle)
+                {
+                    if(e.X > clickPoint.X)
+                    {
+                        if(e.Y > clickPoint.Y)
+                        {
+                            rect = new Rectangle(clickPoint.X, clickPoint.Y, (int)w, (int)h);
+                        }
+                        else
+                        {
+                            rect = new Rectangle(clickPoint.X, e.Y, (int)w, (int)h);
+                        }
+                    }
+                    else
+                    {
+                        if (e.Y > clickPoint.Y)
+                        {
+                            rect = new Rectangle(e.X, clickPoint.Y, (int)w, (int)h);
+                        }
+                        else
+                        {
+                            rect = new Rectangle(e.X, e.Y, (int)w, (int)h);
+                        }
+                    }
+                }
+                else if(tooltype == PaintTools.DrawCircle)
+                {
+                    if (e.X > clickPoint.X)
+                    {
+                        if (e.Y > clickPoint.Y)
+                        {
+                            rect = new Rectangle(clickPoint.X, clickPoint.Y, (int)w, (int)h);
+                        }
+                        else
+                        {
+                            rect = new Rectangle(clickPoint.X, e.Y, (int)w, (int)h);
+                        }
+                    }
+                    else
+                    {
+                        if (e.Y > clickPoint.Y)
+                        {
+                            rect = new Rectangle(e.X, clickPoint.Y, (int)w, (int)h);
+                        }
+                        else
+                        {
+                            rect = new Rectangle(e.X, e.Y, (int)w, (int)h);
+                        }
+                    }
+                }
+                else if(tooltype == PaintTools.Drawline)
+                {
+                    rect = new Rectangle(clickPoint.X, clickPoint.Y, clickPoint.X + e.X, clickPoint.Y + e.Y);
+                }
+
+                listRect.Add(rect);
+                listTool.Add(tooltype);
+                DrawBitmap();
+            }
+        }
+
+        private void DrawBitmap()
+        {
+            if(originalBmp != null)
+            {
+                drawBmp = (Bitmap)originalBmp.Clone();
+
+                for(int i = 0; i < listTool.Count; i++)
+                {
+                    double wRatio = (double)originalBmp.Width / pictureBox1.Width;
+                    double hRatio = (double)originalBmp.Height / pictureBox1.Height;
+
+                    Rectangle rect = new Rectangle((int)(listRect[i].X * wRatio), (int)(listRect[i].Y * hRatio),
+                                                    (int)(listRect[i].Width * wRatio), (int)(listRect[i].Height * hRatio));
+                    Pen pn = new Pen(Color.Black);
+                    pn.Width = (float)(3 * wRatio);
+
+                    using (Graphics g = Graphics.FromImage(drawBmp))
+                    {
+                        if (listTool[i] == PaintTools.DrawRectangle) g.DrawRectangle(pn, rect);
+                        else if (listTool[i] == PaintTools.DrawCircle) g.DrawEllipse(pn, rect);
+                        else if (listTool[i] == PaintTools.Drawline) g.DrawLine(pn, new Point(rect.X, rect.Y), new Point(rect.Width - rect.X, rect.Height - rect.Y));
+                    }
+                }
+
+                pictureBox1.Image = drawBmp;
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if(originalBmp != null)
+            {
+                e.Graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                if(listRect.Count > 0 && drawBmp != null)
+                {
+                    e.Graphics.DrawImage(drawBmp, imgRect);
+                }
+                else
+                {
+                    e.Graphics.DrawImage(originalBmp, imgRect);
+                }
+            }
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(listRect.Count > 0 && listRect.Count <= listRect.Count + tempRect.Count)
+            {
+                tempRect.Add(listRect[listRect.Count - 1]);
+                listRect.RemoveAt(listRect.Count - 1);
+                tempTool.Add(listTool[listTool.Count - 1]);
+                listTool.RemoveAt(listTool.Count - 1);
+                pictureBox1.Refresh();
+                DrawBitmap();
+            }
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (tempRect.Count >= 0 && listRect.Count < listRect.Count + tempRect.Count)
+            {
+                listRect.Add(tempRect[tempRect.Count - 1]);
+                tempRect.RemoveAt(tempRect.Count - 1);
+                listTool.Add(tempTool[tempTool.Count - 1]);
+                tempTool.RemoveAt(tempTool.Count - 1);
+                pictureBox1.Refresh();
+                DrawBitmap();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Control && !e.Shift && e.KeyCode == Keys.Z)
+            {
+                if(listRect.Count > 0 && listRect.Count <= listRect.Count + tempRect.Count)
+                {
+                    tempRect.Add(listRect[listRect.Count - 1]);
+                    listRect.RemoveAt(listRect.Count - 1);
+                    tempTool.Add(listTool[listTool.Count - 1]);
+                    listTool.RemoveAt(listTool.Count - 1);
+                    pictureBox1.Refresh();
+                    DrawBitmap();
+                }
+            }
+            if(e.Control && e.Shift && e.KeyCode == Keys.Z)
+            {
+                if(listRect.Count >= 0 && listRect.Count < listRect.Count + tempRect.Count)
+                {
+                    listRect.Add(tempRect[tempRect.Count - 1]);
+                    tempRect.RemoveAt(tempRect.Count - 1);
+                    listTool.Add(tempTool[tempTool.Count - 1]);
+                    tempTool.RemoveAt(tempTool.Count - 1);
+                    pictureBox1.Refresh();
+                    DrawBitmap();
+                }
+            }
         }
     }
 }
