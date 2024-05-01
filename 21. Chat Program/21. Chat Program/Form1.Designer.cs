79f05d865e7c39527fad15b2714aac798c785fbc
PR5GRAMM2R <pon07151@gmail.com>
2024-05-01 13:50:09 +0900

    --all

--- 21. Chat Program/21. Chat Program/Form1.Designer.cs
+++ 21. Chat Program/21. Chat Program/Form1.Designer.cs
@@ -31,9 +31,9 @@
             this.txt_All = new System.Windows.Forms.TextBox();
             this.txt_Send = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.btn_Server = new System.Windows.Forms.Button();
-            this.btn_Connect = new System.Windows.Forms.Button();
             this.txt_ServerIP = new System.Windows.Forms.TextBox();
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.btn_Server = new System.Windows.Forms.Button();
             this.btn_Exit = new System.Windows.Forms.Button();
             this.btn_Send = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
@@ -41,86 +41,101 @@
             // 
             // txt_All
             // 
-            this.txt_All.Location = new System.Drawing.Point(44, 57);
+            this.txt_All.Location = new System.Drawing.Point(48, 66);
+            this.txt_All.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txt_All.Multiline = true;
             this.txt_All.Name = "txt_All";
-            this.txt_All.Size = new System.Drawing.Size(803, 650);
+            this.txt_All.Size = new System.Drawing.Size(883, 758);
             this.txt_All.TabIndex = 0;
             // 
             // txt_Send
             // 
-            this.txt_Send.Location = new System.Drawing.Point(44, 789);
+            this.txt_Send.Location = new System.Drawing.Point(48, 920);
+            this.txt_Send.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txt_Send.Name = "txt_Send";
-            this.txt_Send.Size = new System.Drawing.Size(803, 28);
+            this.txt_Send.Size = new System.Drawing.Size(883, 32);
             this.txt_Send.TabIndex = 1;
+            this.txt_Send.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_Send_KeyDown);
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.txt_ServerIP);
             this.groupBox1.Controls.Add(this.btn_Connect);
             this.groupBox1.Controls.Add(this.btn_Server);
-            this.groupBox1.Location = new System.Drawing.Point(986, 129);
+            this.groupBox1.Location = new System.Drawing.Point(1085, 150);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(392, 402);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(431, 469);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "옵션";
             // 
-            // btn_Server
+            // txt_ServerIP
             // 
-            this.btn_Server.Location = new System.Drawing.Point(90, 73);
-            this.btn_Server.Name = "btn_Server";
-            this.btn_Server.Size = new System.Drawing.Size(228, 71);
-            this.btn_Server.TabIndex = 0;
-            this.btn_Server.Text = "서버 켜기";
-            this.btn_Server.UseVisualStyleBackColor = true;
+            this.txt_ServerIP.Location = new System.Drawing.Point(99, 354);
+            this.txt_ServerIP.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_ServerIP.Name = "txt_ServerIP";
+            this.txt_ServerIP.Size = new System.Drawing.Size(250, 32);
+            this.txt_ServerIP.TabIndex = 2;
             // 
             // btn_Connect
             // 
-            this.btn_Connect.Location = new System.Drawing.Point(90, 170);
+            this.btn_Connect.Location = new System.Drawing.Point(99, 198);
+            this.btn_Connect.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_Connect.Name = "btn_Connect";
-            this.btn_Connect.Size = new System.Drawing.Size(228, 71);
+            this.btn_Connect.Size = new System.Drawing.Size(251, 83);
             this.btn_Connect.TabIndex = 1;
-            this.btn_Connect.Text = "연결 끊기";
+            this.btn_Connect.Text = "서버 연결";
             this.btn_Connect.UseVisualStyleBackColor = true;
             // 
-            // txt_ServerIP
+            // btn_Server
             // 
-            this.txt_ServerIP.Location = new System.Drawing.Point(90, 303);
-            this.txt_ServerIP.Name = "txt_ServerIP";
-            this.txt_ServerIP.Size = new System.Drawing.Size(228, 28);
-            this.txt_ServerIP.TabIndex = 2;
+            this.btn_Server.Location = new System.Drawing.Point(99, 85);
+            this.btn_Server.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_Server.Name = "btn_Server";
+            this.btn_Server.Size = new System.Drawing.Size(251, 83);
+            this.btn_Server.TabIndex = 0;
+            this.btn_Server.Text = "서버 켜기";
+            this.btn_Server.UseVisualStyleBackColor = true;
+            this.btn_Server.Click += new System.EventHandler(this.btn_Server_Click);
             // 
             // btn_Exit
             // 
-            this.btn_Exit.Location = new System.Drawing.Point(1076, 611);
+            this.btn_Exit.Location = new System.Drawing.Point(1184, 713);
+            this.btn_Exit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_Exit.Name = "btn_Exit";
-            this.btn_Exit.Size = new System.Drawing.Size(228, 71);
+            this.btn_Exit.Size = new System.Drawing.Size(251, 83);
             this.btn_Exit.TabIndex = 3;
             this.btn_Exit.Text = "프로그램 종료";
             this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
             // 
             // btn_Send
             // 
-            this.btn_Send.Location = new System.Drawing.Point(1076, 746);
+            this.btn_Send.Location = new System.Drawing.Point(1184, 870);
+            this.btn_Send.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_Send.Name = "btn_Send";
-            this.btn_Send.Size = new System.Drawing.Size(228, 71);
+            this.btn_Send.Size = new System.Drawing.Size(251, 83);
             this.btn_Send.TabIndex = 4;
             this.btn_Send.Text = "보내기";
             this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1458, 890);
+            this.ClientSize = new System.Drawing.Size(1604, 1038);
             this.Controls.Add(this.btn_Send);
             this.Controls.Add(this.btn_Exit);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.txt_Send);
             this.Controls.Add(this.txt_All);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
--- 21. Chat Program/21. Chat Program/Form1.cs
+++ 21. Chat Program/21. Chat Program/Form1.cs
@@ -15,9 +15,227 @@ namespace _21.Chat_Program
 {
     public partial class Form1 : Form
     {
+        public NetworkStream m_Stream;
+        public StreamReader m_Read;
+        public StreamWriter m_Write;
+        const int PORT = 2002;
+        private Thread m_ThReader;
+
+        public bool m_bStop = false;
+        private TcpListener m_Listener;
+        private Thread m_ThServer;
+
+        public bool m_bConnect = false;
+        TcpClient m_Client;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ServerStop();
+            Disconnect();
+        }
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public void Message(string msg)
+        {
+            this.Invoke(new MethodInvoker(delegate ()
+            {
+                txt_All.AppendText(msg + "\r\n");
+                txt_All.Focus();
+                txt_All.ScrollToCaret();
+                txt_Send.Focus();
+            }));
+        }
+
+        public void ServerStart()
+        {
+            try
+            {
+                m_Listener = new TcpListener(PORT);
+                m_Listener.Start();
+
+                m_bStop = true;
+                Message("클라이언트 접속 대기중");
+
+                while (m_bStop)
+                {
+                    TcpClient hClient = m_Listener.AcceptTcpClient();
+
+                    if (hClient.Connected)
+                    {
+                        m_bConnect = true;
+                        Message("클라이언트 접속");
+
+                        m_Stream = hClient.GetStream();
+                        m_Read = new StreamReader(m_Stream);
+                        m_Write = new StreamWriter(m_Stream);
+
+                        m_ThReader = new Thread(new ThreadStart(Receive));
+                        m_ThReader.Start();
+                    }
+                }
+            }
+            catch
+            {
+                Message("시작 도중에 오류 발생");
+                return;
+            }
+        }
+
+        public void ServerStop()
+        {
+            if (!m_bStop)
+                return;
+
+            m_Listener.Stop();
+
+            m_Read.Close();
+            m_Write.Close();
+
+            m_Stream.Close();
+
+            m_ThReader.Abort();
+            m_ThServer.Abort();
+
+            Message("서비스 종료");
+        }
+        public void Disconnect()
+        {
+            if (!m_bConnect)
+                return;
+
+            m_bConnect = false;
+
+            m_Read.Close();
+            m_Write.Close();
+
+            m_Stream.Close();
+            m_ThReader.Abort();
+
+            Message("상대방과 연결 중단");
+        }
+
+        public void Connect()
+        {
+            m_Client = new TcpClient();
+
+            try
+            {
+                m_Client.Connect(txt_ServerIP.Text, PORT);
+            }
+            catch
+            {
+                m_bConnect = false;
+                return;
+            }
+
+            m_bConnect = true;
+            Message("서버에 연결");
+
+            m_Stream = m_Client.GetStream();
+
+            m_Read = new StreamReader(m_Stream);
+            m_Write = new StreamWriter(m_Stream);
+
+            m_ThReader = new Thread(new ThreadStart(Receive));
+            m_ThReader.Start();
+        }
+
+        public void Receive()
+        {
+            try
+            {
+                while (m_bConnect)
+                {
+                    string szMessage = m_Read.ReadLine();
+
+                    if (szMessage != null)
+                    {
+                        Message("상대방 >>> : " + szMessage);
+                    }
+                }
+            }
+            catch
+            {
+                Message("데이터를 읽는 과정에서 오류가 발생");
+            }
+
+            Disconnect();
+        }
+
+        public void Send()
+        {
+            try
+            {
+                m_Write.WriteLine(txt_Send.Text);
+                m_Write.Flush();
+
+                Message(">>> : " + txt_Send.Text);
+                txt_Send.Text = "";
+            }
+            catch
+            {
+                Message("메시지 전송 실패");
+            }
+        }
+
+        private void btn_Server_Click(object sender, EventArgs e)
+        {
+            if (btn_Server.Text == "서버 켜기")
+            {
+                m_ThServer = new Thread(new ThreadStart(ServerStart));
+                m_ThServer.Start();
+
+                btn_Server.Text = "서버 멈춤";
+                btn_Server.ForeColor = Color.Red;
+            }
+            else
+            {
+                ServerStop();
+                btn_Server.Text = "서버 켜기";
+                btn_Server.ForeColor = Color.Black;
+            }
+        }
+
+        private void btn_Send_Click(object sender, EventArgs e)
+        {
+            Send();
+        }
+
+        private void txt_Send_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Send();
+            }
+        }
+
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            if (btn_Connect.Text == "서버 연결")
+            {
+                Connect();
+
+                if (m_bConnect)
+                {
+                    btn_Connect.Text = "연결 끊기";
+                    btn_Connect.ForeColor = Color.Red;
+                }
+            }
+            else
+            {
+                Disconnect();
+                btn_Connect.Text = "서버 연결";
+                btn_Connect.ForeColor = Color.Black;
+            }
         }
     }
 }
