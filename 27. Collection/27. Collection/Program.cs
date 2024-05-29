5d8c2a9c87f9e043c3ddccba2d5747d586145f92
PR5GRAMM2R <pon07151@gmail.com>
2024-05-29 10:26:40 +0900

    --all

---
+++ 27. Collection/27. Collection/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+namespace Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList myAL = new ArrayList();
+            myAL.Add("Hello");
+            myAL.Add("World");
+            myAL.Add("!");
+
+            Console.WriteLine("Count : {0}", myAL.Count);
+            Console.WriteLine("Capacity : {0}", myAL.Capacity);
+            Console.Write("Values :");
+            PrintValues(myAL);
+        }
+
+        static void PrintValues(IEnumerable myList)
+        {
+            foreach (Object obj in myList)
+            {
+                Console.Write(" {0}", obj);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 28. IEnumerable/28. IEnumerable/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+
+namespace aaa
+{
+    class Program
+    {
+        public class Person 
+        {
+            public Person(string fName, string lName)
+            {
+                this.firstName = fName;
+                this.lastName = lName;
+            }
+
+            public string firstName;
+            public string lastName;
+        }
+
+        public class People : IEnumerable
+        {
+            private Person[] _people;
+
+            public People(Person[] pArray)
+            {
+                _people = new Person[pArray.Length];
+
+                for(int i = 0; i < pArray.Length; i++)
+                {
+                    _people[i] = pArray[i];
+                }
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                return new PeopleEnum(_people);
+            }
+        }
+
+        public class PeopleEnum : IEnumerator
+        {
+            public Person[] _people;
+
+            int position = -1;
+
+            public PeopleEnum(Person[] list)
+            {
+                _people = list;
+            }
+
+            public bool MoveNext()
+            {
+                position++;
+                return (position < _people.Length);
+            }
+
+            public void Reset()
+            {
+                position = -1;
+            }
+
+            public object Current
+            {
+                get
+                {
+                    try
+                    {
+                        return _people[position];
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Person[] peopleArray = new Person[3]
+            {
+                new Person("A", "a"),
+                new Person("B", "b"),
+                new Person("C", "c"),
+            };
+
+            People peopleList = new People(peopleArray);
+            foreach(Person person in peopleList)
+            {
+                Console.WriteLine(person.firstName + " " + person.lastName);
+            }
+        }
+    }
+}
